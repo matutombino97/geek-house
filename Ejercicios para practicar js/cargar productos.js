69220ba8cc8aaca6fd66e310b5f585d09af979f2
Matias <matuprograma97@gmail.com>
2026-01-14 00:20:24 -0300

    Readaptando el nav

--- Ejercicios para practicar js/cargar productos.js
+++
@@ -1,7 +0,0 @@
-function analizarUrl(urlFicticia = "noticia.html?id=55&categoria=deportes&autor=messi"){
-    const url = URLSearchParams(urlFicticia);
-    const datos_url = url.get("id");
-    return datos_url;
-}
-
-console.log(datos_url)
